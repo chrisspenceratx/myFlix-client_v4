a6fa7cc54fa0cf3167079c7c4b194471196d3a61
chrisspenceratx <chrisspenceratx@gmail.com>
2023-03-03 17:13:18 -0600

    3.4 edit

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,7 +9,7 @@ export const LoginView = ({ onLoggedIn }) => {
 
     const handleSubmit = (event) => {
       // this prevents the default behavior of the form which is to reload the entire page//
-      // simple edit for 3.4 resubmission/merge //
+      // simple edit for 3.4 resubmission/merge right here //
 
       event.preventDefault();
   
